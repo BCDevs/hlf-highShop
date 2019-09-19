e667e42f71a633dfa322c702053a138ff0530508
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-19 16:23:59 +0530

    Update chaincode.js

--- chaincode/newcc/chaincode.js
+++ chaincode/newcc/chaincode.js
@@ -122,6 +122,37 @@ let credentialsAsBytes = await ctx.stub.getState(ownerId);
             console.log('Patent Successfully Proposed With an Id of Pat'+refId);
         }
 }
+  async queryAllPatents(ctx) {
+        const startKey = 'Pat1';
+        const endKey = 'Pat999';
+
+        const iterator = await ctx.stub.getStateByRange(startKey, endKey);
+
+        const allResults = [];
+        while (true) {
+            const res = await iterator.next();
+
+            if (res.value && res.value.value.toString()) {
+                console.log(res.value.value.toString('utf8'));
+
+                const Key = res.value.key;
+                let Record;
+                try {
+                    Record = JSON.parse(res.value.value.toString('utf8'));
+                } catch (err) {
+                    console.log(err);
+                    Record = res.value.value.toString('utf8');
+                }
+                allResults.push({ Key, Record });
+            }
+            if (res.done) {
+                console.log('end of data');
+                await iterator.close();
+                console.info(allResults);
+                return JSON.stringify(allResults);
+            }
+        }
+    }
 
 async verifyPatent(ctx,verifierId,key, patentId) {
 
